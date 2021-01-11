77d6848cb21a609519ed1c6ab8b2f99c2e68b5d3
julielhote <julie.lhote@firstroot.co>
2021-01-11 17:36:39 +0100

    added data from the last five days

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from libs.openweathermap import CurrentWeather
+from libs.openweathermap import CurrentWeather, HistoricalWeatherData
+from datetime import datetime
 
 API_KEY = "813a7ac588302e96a5b068466ae2944f"
-city_name = input("Enter the city you want to have information from: ")
-current_weather = CurrentWeather(API_KEY, city_name).latest()
 
 USER_CHOICE = """Enter:
 - 'coord' to know the coordinates
@@ -10,6 +9,7 @@ USER_CHOICE = """Enter:
 - 'main' to know the temperature
 - 'visibility' to know the visibility
 - 'wind' for information of the wind
+- 'hd' for Data from the last 5 days
 - 'q' to quit
 
 Your choice: """
@@ -18,12 +18,34 @@ Your choice: """
 def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
-        print_information(user_input)
+        if user_input == 'coord' or user_input == 'weather' \
+                or user_input == 'main' or user_input == 'visibility' or user_input == 'wind':
+            print_current_information(user_input)
+        elif user_input == 'hd':
+            print_historical_data()
+        else:
+            print("Unknown command! Please try again")
         user_input = input(USER_CHOICE)
 
 
-def print_information(user_input):
+def print_current_information(user_input):
+    city_name = input("Enter the city you want to have information from: ")
+    current_weather = CurrentWeather(API_KEY, city_name).latest()
     return print(current_weather[user_input])
+
+
+def print_historical_data():
+    city_lat = input("Enter the latitude of the city you want to have information from: ")
+    city_lon = input("Enter the longitude of the city you want to have information from: ")
+    day = int(input("Enter from what day you want the information: "))
+    historical_weather = HistoricalWeatherData(API_KEY, city_lat, city_lon, convert_time(2021, 1, day))
+    return print(historical_weather.latest())
+
+
+def convert_time(year, month, day):
+    dt = datetime(year, month, day)
+    ts = int(datetime.timestamp(dt))
+    return ts
 
 
 menu()
--- libs/openweathermap.py
+++ libs/openweathermap.py
@@ -10,3 +10,16 @@ class CurrentWeather:
 
     def latest(self):
         return requests.get(f"{self.BASE_URL}q={self.city_name}&appid={self.api_key}").json()
+
+
+class HistoricalWeatherData:
+    BASE_URL = "https://api.openweathermap.org/data/2.5/onecall/timemachine?"
+
+    def __init__(self, api_key, lat, lon, time):
+        self.api_key = api_key
+        self.lat = lat
+        self.lon = lon
+        self.time = time
+
+    def latest(self):
+        return requests.get(f"{self.BASE_URL}lat={self.lat}&lon={self.lon}&dt={self.time}&appid={self.api_key}").json()
